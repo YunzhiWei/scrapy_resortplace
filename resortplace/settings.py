38d8cd3f1311dba0d756fe3a39c0dd548fd98f96
Chris Wei <yunzhi.wei@live.com>
2016-12-16 14:08:26 +0800

    Setting for delay

--- resortplace/settings.py
+++ resortplace/settings.py
@@ -27,10 +27,14 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1.5
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 6
 #CONCURRENT_REQUESTS_PER_IP = 16
+
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 2
+AUTOTHROTTLE_TARGET_CONCURRENCY = 6
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
